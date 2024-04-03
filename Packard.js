0b81326dc4da625360dd8227dfa190f3acb6cdaf
momyhub <165927126+momyhub@users.noreply.github.com>
2024-04-03 15:22:09 +0000

    Create package.js

---
+++ Packard.js
@@ -0,0 +1,20 @@
+
+{
+  "name": "botc1",
+  "version": "1.0.0",
+  "description": "bot accueil ",
+  "main": "bot.js",
+  
+  "dependencies": {
+    "node-telegram-bot-api": "^0.59.0",
+    "lodash": "^4.17.21",
+    "node-schedule": "^1.3.2"
+  
+
+  },
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "author": "solkah",
+  "license": "MIT"
+  }
